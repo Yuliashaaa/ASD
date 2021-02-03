43645f45fec6639361aac2946eba64bebc17cbc3
Yuliashaaa <ulka.shevchenko@gmail.com>
2021-02-03 13:07:53 +0200

    Update Task_1.cpp

--- Lab_1/Task_1.cpp
+++ Lab_1/Task_1.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 using namespace std;
 
-double power(double m, int n);
+double power(double m, int n){
+  double res;
+  if (n == 0)
+    return 1;
+  else if (n == 1)
+    return m;
+  res = m * power(m, n - 1);
+  return res;
+}
 
 int main(){
   double x;
@@ -12,14 +20,4 @@ int main(){
   cin >> y;
   cout << "\n Result of " << x << " to the power of " << y << " is " << power(x, y);
   return 0;
-}
-
-double power(double m, int n){
-  double res;
-  if (n == 0)
-    return 1;
-  else if (n == 1)
-    return m;
-  res = m * power(m, n - 1);
-  return res;
 }
