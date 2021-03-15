216500bc56b0ba8c6daf22664f89170c65cc76ab
Yuliashaaa <ulka.shevchenko@gmail.com>
2021-03-15 20:27:51 +0200

    Update Bubble_Sort.cpp

--- Lab_2/Bubble_Sort.cpp
+++ Lab_2/Bubble_Sort.cpp
@@ -1,27 +1,37 @@
 #include <iostream>
+#include <chrono>
 using namespace std;
 
-int main(){
-  int a[7] = {22, 20, -1, -40, 88, -75, -22}; //Starting array
-  int size = 7; //Array size
-  int temp; //Variable for element swap
-  bool exit = false; //Variable to check swaps;
+int main() {
+    int a[7] = { 22, 20, -1, -40, 88, -75, -22 }; // Starting array
+    int size = 7; // Array size
+    int temp; // Variable for element swap
+    bool exit = false; // Variable to check swaps;
 
-  while(!exit){
-    exit = true;
-    for(int i = 0; i < size - 1; i++){
-      for(int j = 0; j < size - 1 - i; j++){
-        if(a[j] > a[j + 1]){
-          temp = a[j];
-          a[j] = a[j + 1];
-          a[j + 1] = temp;
-          exit = false;
+    auto start = std::chrono::steady_clock::now();
+
+    while (!exit) {
+        exit = true;
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - 1 - i; j++) {
+                if (a[j] > a[j + 1]) {
+                    temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                    exit = false;
+                }
+            }
         }
-      }
     }
-  }
 
-  for(int i = 0; i < size; i++){
-    cout << a[i] << " ";
-  }
+    auto finish = std::chrono::steady_clock::now();
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < size; i++) {
+        cout << a[i] << " ";
+    }
+
+    auto time = std::chrono::duration_cast<std::chrono::microseconds>(finish - start);
+    cout << endl;
+    cout << "Algorithm work time : " << time.count() << " microseconds";
 }
